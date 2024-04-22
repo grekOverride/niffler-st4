75e44e0af7be534a00dde198dcd19e653c42ed3b
kateoverride <kstrelkova@t1-consulting.ru>
2024-04-22 23:28:13 +0300

    add part 2 of hw4

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserRepositoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/UserRepositoryExtension.java
@@ -2,6 +2,8 @@ package guru.qa.niffler.jupiter.extension;
 
 import guru.qa.niffler.db.repository.UserRepository;
 import guru.qa.niffler.db.repository.UserRepositoryHibernate;
+import guru.qa.niffler.db.repository.UserRepositoryJdbc;
+import guru.qa.niffler.db.repository.UserRepositorySJdbc;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 
@@ -13,7 +15,18 @@ public class UserRepositoryExtension implements TestInstancePostProcessor {
     for (Field field : o.getClass().getDeclaredFields()) {
       if (field.getType().isAssignableFrom(UserRepository.class)) {
         field.setAccessible(true);
-        field.set(o, new UserRepositoryHibernate());
+
+        UserRepository userRepository;
+
+        /*if ("hibernate".equals(System.getProperty("db.impl"))) {
+          userRepository = new UserRepositoryHibernate();
+        } else */
+        if ("sjdbc".equals(System.getProperty("repository"))) {
+          userRepository = new UserRepositorySJdbc();
+        } else {
+          userRepository = new UserRepositoryJdbc();
+        }
+        field.set(o, userRepository);
       }
     }
   }
